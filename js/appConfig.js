674ea0658c9b25e92b85124471a63f331c778c96
Nicolas Dufrane <nicolas.dufrane@opengeode.be>
2016-06-30 15:07:55 +0200

    initial import

---
+++ js/appConfig.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2016, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+    pages: [{
+        name: "home",
+        path: "/",
+        component: require('./pages/Home')
+    }, {
+        name: "home2",
+        path: "/main2",
+        component: require('./pages/Main')
+    }, {
+        name: "main",
+        path: "/main",
+        component: require('./pages/Main')
+    }],
+    pluginsDef: require('./plugins.js'),
+    initialState: {
+        defaultState: {},
+        mobile: {}
+    }
+};
---
+++ js/plugins.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright 2016, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+    plugins: {
+        MapPlugin: require('../MapStore2/web/client/plugins/Map'),
+        //MapPlugin: require('js/components/MapPlugin'),
+        HomePlugin: require('../MapStore2/web/client/plugins/Home'),
+        TOCPlugin: require('../MapStore2/web/client/plugins/TOC'),
+        ScaleBoxPlugin: require('../MapStore2/web/client/plugins/ScaleBox'),
+        ZoomAllPlugin: require('../MapStore2/web/client/plugins/ZoomAll'),
+        ToolbarPlugin: require('../MapStore2/web/client/plugins/Toolbar'),
+        BackgroundSwitcherPlugin: require('../MapStore2/web/client/plugins/BackgroundSwitcher'),
+        IdentifyPlugin: require('../MapStore2/web/client/plugins/Identify'),
+        MeasurePlugin: require('../MapStore2/web/client/plugins/Measure'),
+        SettingsPlugin: require('../MapStore2/web/client/plugins/Settings'),
+        MousePositionPlugin: require('../MapStore2/web/client/plugins/MousePosition'),
+    },
+    requires: {}
+};
