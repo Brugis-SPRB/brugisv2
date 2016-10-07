6e58739ac9f5f0e5b55860a4c4768dadd2dd3e8b
Nicolas Dufrane <nicolas.dufrane@opengeode.be>
2016-10-07 16:31:06 +0200

    treeview UI

--- js/plugins/BrugisTree/BrugisTree.jsx
+++ js/plugins/BrugisTree/BrugisTree.jsx
@@ -4,22 +4,72 @@ const {connect} = require('react-redux');
 const layersIcon = require('../../../MapStore2/web/client/plugins/toolbar/assets/img/layers.png');
 const {loadBrugisTreeConfig} = require('./actions');
 
+const BrugisTreeNode = React.createClass({
+  toggle: function(path) {
+      this.props.toggle(path);
+  },
+
+  toggleLayer : function(layerName) {
+      this.props.toggleLayer(layerName);
+  },
+
+  shouldComponentUpdate: function(nextProps, nextState) {
+    return ! (nextProps.item ==  this.props.item)
+  },
+
+  divStyle: function(item) {
+    //var indent    = item.path.size;
+    return {
+      marginLeft: (1 * 1) + "px"
+    };
+  },
+  renderChild: function(item) {
+    return (
+        <ul className="tree">
+            <BrugisTreeNode item={item} toggle={this.props.toggle} />
+        </ul>
+    )
+  },
+  render: function() {
+    var item = this.props.item
+    var disabled  = item.childNodes == null || item.childNodes.length == 0;
+    var collapsed = ! item.expanded;
+    var text      = disabled ? "" : collapsed ? "+" : "-";
+
+    return (<li className="tree">
+               <input type="checkbox" disabled={disabled} onClick={this.toggleLayer.bind(this, item.path)}  style={this.divStyle(item)}></input>
+                   <i className="fa fa-angle-double-right">+</i>
+                   <i className="fa fa-angle-double-down">-</i>
+                <label onClick={this.toggle.bind(this, item.path)}>{item.title}</label>
+          {(item.expanded && item.childNodes) ? item.childNodes.map(this.renderChild, this) : false }
+      </li>)
+  }
+});
+
 const BrugisTree = React.createClass({
     propTypes: {
         expanded: React.PropTypes.bool,
-        loadTreeData: React.PropTypes.func
+        loadTreeData: React.PropTypes.func,
+        treenodes : React.PropTypes.array
     },
     getDefaultProps() {
         return {
             expanded: true,
-            loadTreeData: () => {}
+            loadTreeData: () => {},
+            treenodes : []
         };
     },
     componentWillMount() {
         this.props.loadTreeData();
     },
+    toggle: function(path) {
+        console.log(path);
+    },
+    toggleLayer: function(path) {
+        console.log(path);
+    },
     render() {
-        if (!this.props.brugistreedata) {
+        if (!this.props.treenodes) {
             return (
                 <ul>
                     <li>NO DATA</li>
@@ -27,10 +77,10 @@ const BrugisTree = React.createClass({
             )
         }
         return (
-            <ul>
+            <ul className="tree">
                 {
-                    this.props.brugistreedata.map((alayer) => {
-                        return <li>alayer</li>;
+                    this.props.treenodes.map((node) => {
+                        return <BrugisTreeNode item={node} toggle={this.props.toggle} toggleLayer={this.props.toggleLayer} />;
                     })
                 }
             </ul>
@@ -39,7 +89,9 @@ const BrugisTree = React.createClass({
 });
 
 
-const BrugisTreePlugin = connect(() => ({}), {
+const BrugisTreePlugin = connect((state) => ({
+    treenodes: state && state.brugisTree && state.brugisTree.treenodes
+}), {
     loadTreeData: loadBrugisTreeConfig
 })(BrugisTree);
 
--- js/plugins/BrugisTree/reducers.jsx
+++ js/plugins/BrugisTree/reducers.jsx
@@ -1,12 +1,38 @@
 
 var {BRUGIS_TREE_LOAD_START, BRUGIS_TREE_LOADED, BRUGIS_TREE_LOAD_ERROR} = require('./actions');
+const assign = require('object-assign');
+
+function wmsWalker(layers) {
+    if(typeof(layers) == "undefined" || layers == null || layers.length == 0) {
+        return [];
+    } else {
+        var tmp_nodes = [];
+        for(var i=0; i<layers.length; i++){
+            var node = {};
+            var cur_layer = layers[i];
+            node.expanded = true;
+            node.name = cur_layer.name;
+            node.title = cur_layer.title;
+            node.path = true;
+            node.childNodes = wmsWalker(cur_layer.layer);
+            tmp_nodes.push(node);
+        }
+        return tmp_nodes;
+    }
+}
 
 function brugisTree(state = null, action) {
     switch (action.type) {
         case BRUGIS_TREE_LOAD_START:
             return state;
         case BRUGIS_TREE_LOADED:
-            return state;
+            const capabilities =  action.info;
+            var layers = capabilities.value.capability.layer.layer;
+            var nodes = wmsWalker(layers);
+
+            return assign({}, state, {
+                treenodes : nodes
+            });
         case BRUGIS_TREE_LOAD_ERROR:
             return state;
         default:
