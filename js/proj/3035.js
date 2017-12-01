74ef679d0d3eb7fb587c3eeea6e5614e96392180
DocG <gvandenschrick@cirb.irisnet.be>
2017-12-01 14:51:23 +0100

    lint of some file from debilos

--- js/proj/3035.js
+++ js/proj/3035.js
@@ -1,4 +1,4 @@
 
 const proj4 = require('proj4');
 
-proj4.defs("EPSG:3035","+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
+proj4.defs("EPSG:3035", "+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
