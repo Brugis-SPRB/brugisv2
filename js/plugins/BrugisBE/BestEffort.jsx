53d6e73fbcecc73dd16af820d49e1cedc4910a32
Nicolas Dufrane <dufrane.nicolas@gmail.com>
2018-05-18 15:59:16 +0200

    add plugin besteffort

---
+++ js/plugins/BrugisBE/BestEffort.jsx
@@ -0,0 +1,37 @@
+const React = require('react');
+
+const Message = require('../../../MapStore2/web/client/components/I18N/Message');
+
+const assign = require('object-assign');
+const PropTypes = require('prop-types');
+
+class BestEffort extends React.Component {
+    static propTypes = {
+        id: PropTypes.string,
+        style: PropTypes.object,
+        message: PropTypes.string
+    };
+
+    static defaultProps = {
+        id: 'brugis-besteffort',
+        message: 'notinmyname'
+    };
+    render() {
+        return (
+          <div id={this.props.id} style={this.props.style}>
+              <label><Message msgId="best-effort" /></label>
+          </div>
+        );
+    }
+}
+
+module.exports = {
+    BestEffortPlugin: assign(BestEffort, {
+        MapFooter: {
+            name: 'bestEffort',
+            position: 1,
+            tool: true,
+            priority: 1
+        }
+    })
+};
