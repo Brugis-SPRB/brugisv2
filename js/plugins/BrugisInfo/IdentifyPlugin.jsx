3c1490f9438d018ae6962dda16f76ab89b5e8ef4
Nicolas Dufrane <dufrane.nicolas@gmail.com>
2018-06-01 13:19:35 +0200

    cleaning copyright for code outside mapstore2

--- js/plugins/BrugisInfo/IdentifyPlugin.jsx
+++ js/plugins/BrugisInfo/IdentifyPlugin.jsx
@@ -1,10 +1,3 @@
-/*
- * Copyright 2016, GeoSolutions Sas.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree.
- */
 const React = require('react');
 const {connect} = require('react-redux');
 const {createSelector} = require('reselect');
--- js/stores/store.js
+++ js/stores/store.js
@@ -1,10 +1,4 @@
-/**
- * Copyright 2016, GeoSolutions Sas.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree.
- */
+
 const assign = require('object-assign');
 
 const {mapConfigHistory, createHistory} = require('../../MapStore2/web/client/utils/MapHistoryUtils');
