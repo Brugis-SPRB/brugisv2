baa298ccff6ec1092f720bb0f511f4fcc0d49cfa
Nicolas Dufrane <dufrane.nicolas@gmail.com>
2018-08-24 10:22:29 +0200

    Change zoom from zoomlvl to actual scale

--- js/actions/config.js
+++ js/actions/config.js
@@ -3,6 +3,27 @@ const url = require('url');
 
 
 const {configureMap, configureError} = require('../../MapStore2/web/client/actions/config');
+const mapUtils = require('../../MapStore2/web/client/utils/MapUtils');
+const CoordinatesUtils = require('../../MapStore2/web/client/utils/CoordinatesUtils');
+
+const METERS_PER_UNIT = {
+    'm': 1,
+    'degrees': 111194.87428468118,
+    'ft': 0.3048,
+    'us-ft': 1200 / 3937
+};
+
+function dpi2dpu(dpi, projection) {
+    const units = CoordinatesUtils.getUnits(projection || "EPSG:3857");
+    return METERS_PER_UNIT[units] * mapUtils.dpi2dpm(dpi || DEFAULT_SCREEN_DPI);
+}
+
+function getScales(resolutions, projection, dpi) {
+    var dpu = dpi2dpu(dpi, projection);
+    return resolutions.map(function (resolution) {
+        return resolution * dpu;
+    });
+}
 
 /*
   Altered version of loadMapConfig.
@@ -24,8 +45,20 @@ function loadMapConfig(configName, mapId) {
                     response.data.map.center.x = parseInt(queryObj.lambx, 10);
                     response.data.map.center.y = parseInt(queryObj.lamby, 10);
                 }
-                if (queryObj.zoom && parseInt(queryObj.zoom, 10) > 0 && parseInt(queryObj.zoom, 10) < 30) {
-                    response.data.map.zoom = parseInt(queryObj.zoom, 10);
+                if (queryObj.zoom && parseInt(queryObj.zoom, 10) > 0 && parseInt(queryObj.zoom, 10) < 100000) {
+                   var scales = getScales(
+                      response.data.map.mapOptions.view.resolutions,
+                      response.data.map.projection,
+                      response.data.map.mapOptions.view.DPI
+                   );
+                   var zoom = scales.length -1;
+                   for (var zoomLvl in scales) {
+                     if (parseInt(queryObj.zoom, 10) >= Math.round(scales[zoomLvl])) {
+                        zoom = zoomLvl;
+                        break;
+                     }
+                   }
+                    response.data.map.zoom = zoom;
                 }
                 dispatch(configureMap(response.data, mapId));
             } else {
