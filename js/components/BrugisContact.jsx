ef546732f7b721bed9146141dc6ca05ecb0c41a1
ndufrane <dufrane.nicolas@gmail.com>
2019-04-18 15:43:06 +0200

    lint

--- js/components/BrugisContact.jsx
+++ js/components/BrugisContact.jsx
@@ -1,7 +1,7 @@
 
 const React = require('react');
 const PropTypes = require('prop-types');
-const {Grid, Row, Col} = require('react-bootstrap');
+const {Grid, Row, Col, Form, FormGroup, FormControl, Button, Checkbox, ControlLabel} = require('react-bootstrap');
 const Message = require('../../MapStore2/web/client/components/I18N/Message');
 
 class BrugisContact extends React.Component {
@@ -23,8 +23,46 @@ class BrugisContact extends React.Component {
                 <Col md={6}>
                 <h2>Contact</h2><br/>
                 <hr />
-                <img src="https://sayingimages.com/wp-content/uploads/you-have-tech-support-meme.jpg"></img><br />
-                <label><Message msgId="best-effort" /><a href="mailto:brugis@urban.brussels"><Message msgId="BruGISTeam" /></a></label>
+                <Form horizontal action="mailto:brugis@urban.brussels" method="GET">
+                    <FormGroup controlId="formHorizontalEmail">
+                    <Col componentClass={ControlLabel} sm={2}>
+                        Subject
+                    </Col>
+                    <Col sm={10}>
+                        <FormControl type="text" placeholder="Subject" name="subject"/>
+                    </Col>
+                    </FormGroup>
+
+                    <FormGroup controlId="formControlsTextarea">
+                    <Col componentClass={ControlLabel} sm={2}>
+                     Message
+                    </Col>
+                    <Col sm={10}>
+                        <FormControl componentClass="textarea" placeholder="textarea" name="body" />
+                    </Col>
+                    </FormGroup>
+
+                    <FormGroup>
+                    <Col componentClass={ControlLabel} sm={2}>
+                    Urgent
+                    </Col>
+                    <Col sm={10}>
+                        <Checkbox/>
+                    </Col>
+                    </FormGroup>
+
+                    <FormGroup>
+                    <Col smOffset={2} sm={10}>
+                        <Button type="submit">
+                        Send
+                        </Button>
+                    </Col>
+                    </FormGroup>
+                </Form>
+                <br />
+                <Col smOffset={2} sm={10}>
+                    <label><Message msgId="best-effort" /><a href="mailto:brugis@urban.brussels"><Message msgId="BruGISTeam" /></a></label>
+                    </Col>
                 </Col>
                 <Col md={6}>
                 <h2>Trombinoscope</h2><br/>
