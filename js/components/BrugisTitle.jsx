d09693cee6d6ebd394dbc07bc2f6929e609979c6
VANDENSCHRICK <gvandenschrick@urban.brussels>
2019-04-26 14:44:52 +0200

    brugis title added

--- js/components/BrugisTitle.jsx
+++ js/components/BrugisTitle.jsx
@@ -1,8 +1,7 @@
 
 const React = require('react');
 const PropTypes = require('prop-types');
-const {Grid, Row, Col, Form, FormGroup, FormControl, Button, Checkbox, ControlLabel} = require('react-bootstrap');
-const Message = require('../../MapStore2/web/client/components/I18N/Message');
+const {Grid, Row, Col} = require('react-bootstrap');
 
 class BrugisTitle extends React.Component {
    static propTypes = {
@@ -20,22 +19,19 @@ class BrugisTitle extends React.Component {
        return (
            <Grid style={{ height: '60%', width: '100%', fontWeight: 'Medium'}}>
             <Row style={{ fontSize: '8em', lineHeight: '1em', color: '#4f8da8'}}>
-                <Col md={6}>
-                </Col>
+                <Col md={6}/>
                 <Col md={6}>
                 BIENVENUE
                 </Col>
             </Row>
             <Row style={{ fontSize: '5em', lineHeight: '1em', fontWeight: 'Bold', color: '#a2c6d7'}}>
-                <Col md={3}>
-                </Col>
+                <Col md={3}/>
                 <Col md={7}>
                 SUR LA PAGE D'INFORMATION DE
                 </Col>
             </Row>
             <Row style={{ fontSize: '11em', lineHeight: '1em', fontWeight: 'Bold', color: '#345d6f'}}>
-                <Col md={5}>
-                </Col>
+                <Col md={5}/>
                 <Col md={5}>
                 BruGIS!
                 </Col>
