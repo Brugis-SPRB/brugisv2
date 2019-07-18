5462ae2831c650250a4640f9748a448e86bf8be4
ndufrane <dufrane.nicolas@gmail.com>
2019-07-18 14:45:08 +0200

    add archive button

--- js/components/BrugisNewsCarousel.jsx
+++ js/components/BrugisNewsCarousel.jsx
@@ -1,6 +1,6 @@
 const React = require('react');
 const PropTypes = require('prop-types');
-const {Panel, Row, Col, Grid, Image, Carousel} = require('react-bootstrap');
+const {Panel, Row, Col, Grid, Image, Carousel, Button} = require('react-bootstrap');
 const newsFR = require('../../news/brugisnews_fr-FR');
 const newsNL = require('../../news/brugisnews_nl-NL');
 
@@ -10,7 +10,8 @@ class BrugisNews extends React.Component {
         className: PropTypes.string,
         name: PropTypes.string,
         locale: PropTypes.string,
-        contentTabStyle: PropTypes.string
+        contentTabStyle: PropTypes.string,
+        showAllNews: PropTypes.bool
     };
 
     static defaultProps = {
@@ -19,8 +20,17 @@ class BrugisNews extends React.Component {
         locale: 'en-EN',
         contentTabStyle: {
             'min-height': '300px'
-        }
+        },
+        showAllNews: false
     };
+
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            showAllNews: this.props.showAllNews
+        };
+    }
 
     renderNews() {
         let eventCounter = 1;
@@ -67,19 +77,23 @@ class BrugisNews extends React.Component {
 
     renderCarouselItem() {
         let news = (this.props.locale === "fr-FR" || this.props.locale === "fr-BE") ? newsFR : newsNL;
-        
+
         return news.news.reverse().map((newsItem) => {
             let dateEnd = new Date(Date.parse(newsItem.dateend));
             let dateStart = new Date(Date.parse(newsItem.datestart));
             let dateNow = new Date();
-            if (dateStart <= dateNow && dateEnd >= dateNow) {
+            let img = "https://picsum.photos/600/500";
+            if (newsItem.img) {
+                img = require('../../news/imgs/' + newsItem.img);
+            }
+            if (this.showAllNews || (dateStart <= dateNow && dateEnd >= dateNow)) {
                 return (
                    <Carousel.Item>
                     <Grid fluid style={{backgroundColor: 'white', height: '100%', color: '#8d8d8d'}}>
                         <Col md={6}>
-                            <Image src="https://picsum.photos/900/560" responsive />
+                            <Image src={img} responsive />
                         </Col>
-                        <Col md={6} >
+                        <Col md={6}>
                         <h2 >{newsItem.title}</h2>
                         <h4>{dateStart.toLocaleString(this.props.locale, { month: 'long', day: 'numeric'})}</h4>
                         <p style={{"padding": "10px", "lineHeight": '1em'}}>
@@ -93,14 +107,39 @@ class BrugisNews extends React.Component {
         });
     }
 
+    renderAllNewsButton() {
+        const { showAllNews } = this.state;
+        return (
+            <Button id="renderallnews" onClick={this.toggleAllNews.bind(this)}>{showAllNews ? 'Voir les news' : 'Voire les archives'}</Button>
+        );
+    }
+
     render() {
         return (
+            <div>
             <Row>
-                <Carousel>
-                    {this.renderCarouselItem()}
-                </Carousel>
+                <Col md={12}>
+                    <Carousel>
+                        {this.renderCarouselItem()}
+                    </Carousel>
+                </Col>
             </Row>
+            <Row className="justify-content-md-center">
+                <Col md={5} />
+                    <Col md={2} className="text-center">
+                    {this.renderAllNewsButton()}
+                    </Col>
+                <Col md={5} />
+            </Row>
+            </div>
         );
+    }
+
+    toggleAllNews() {
+        const { showAllNews } = this.state;
+        this.setState({
+            showAllNews: !showAllNews
+        });
     }
 }
 
