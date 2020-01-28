ff005981cc870a91c16abec8ca046d5492941ce7
gregvds <gregvds0@gmail.com>
2020-01-28 14:26:25 +0100

    modenv.js fix

--- modenv.js
+++ modenv.js
@@ -53,12 +53,12 @@ if(args.length != 1) {
 
 replaceInFile(
   ["./wmsaatl_en.xml", "./wmsaatl_fr.xml", "./wmsaatl_nl.xml"],
-  "https://mybrugis.irisnet.be/geoserver/wms?SERVICE=WMS&amp;", string(ENV_CONFIG_GEOSERVER[args[0]]+ "?SERVICE=WMS&amp;")
+  "https://mybrugis.irisnet.be/geoserver/wms?SERVICE=WMS&amp;", str(ENV_CONFIG_GEOSERVER[args[0]]+ "?SERVICE=WMS&amp;")
 );
 
 replaceInFile(
   ["./wmsaatl_en.xml", "./wmsaatl_fr.xml", "./wmsaatl_nl.xml"],
-  "https://mybrugis.irisnet.be/geoserver/wms?", string(ENV_CONFIG_GEOSERVER[args[0]]+ "?")
+  "https://mybrugis.irisnet.be/geoserver/wms?", str(ENV_CONFIG_GEOSERVER[args[0]]+ "?")
 );
 
 replaceInFile(
