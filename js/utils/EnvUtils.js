4ac44f8180e5411cc434b4dab52413f0b1970597
Grégoire Vandenschrick <gregvds0@gmail.com>
2020-02-01 10:31:24 +0100

    envutils for STANEW

--- js/utils/EnvUtils.js
+++ js/utils/EnvUtils.js
@@ -1,18 +1,23 @@
 
 const DEV = 'DEV';
 const STA = 'STA';
+const STANEW = 'STANEW';
 const PRD = 'PRD';
 
 const EnvUtils = {
 
     getEnvironment: function() {
         var hostname = window.location.hostname;
+        console.log(hostname);
         var ENV = DEV;
         if (hostname.indexOf('irisnet.be') >= 0) {
             ENV = PRD;
         }
         if (hostname.indexOf('irisnetlab.be') >= 0) {
             ENV = STA;
+        }
+        if (hostname.indexOf('10.128.91.10') >= 0) {
+            ENV = STANEW;
         }
         return ENV;
     },
@@ -26,6 +31,9 @@ const EnvUtils = {
                 break;
             case STA:
                 url = "mybrugis.irisnetlab.be/geoserver/ows";
+                break;
+            case STANEW:
+                url = "10.128.91.10:8080/geoserver/ows";
                 break;
             case PRD:
                 url = "mybrugis.irisnet.be/geoserver/ows";
