7e7c71606d54e9a149cf12d4253c2582effc0a44
Grégoire Vandenschrick <gregvds0@gmail.com>
2020-06-03 10:37:30 +0200

    news migration

--- news/brugisnews_fr-FR.js
+++ news/brugisnews_fr-FR.js
@@ -3,83 +3,12 @@ const news = {
     "locale": "fr-FR",
     "news": [
       {
-        "datestart": "2020-02-05",
-        "dateend": "2020-04-01",
+        "datestart": "2020-06-01",
+        "dateend": "2020-06-15",
         "defcon": "5",
-        "title": "BruGIS victime de son succès...",
+        "title": "Migration d'infrastructure BruGIS",
         "img": 'brugis.png',
-        "message": "Notre plateforme est devenue très lente et difficile d'accès depuis décembre 2019. Nous sommes en cours de recréaction d'une nouvelle architecture de serveur pour retrouver un service et une disponibilité optimale. Nous vous présentons nos excuses pour les désagréments rencontrés"
-      },
-      {
-        "datestart": "2020-02-05",
-        "dateend": "2020-06-01",
-        "defcon": "5",
-        "title": "Une nouvelle zone de revitalisation Urbaine!",
-        "img": 'brugis.png',
-        "message": "La ZRU 2020 est désormais disponible sous le dossier Perspective.brussels!"
-      },
-      {
-          "datestart": "2019-10-01",
-          "dateend": "2020-10-01",
-          "defcon": "5",
-          "title": "Les Orthophotoplans 2019 sont en ligne!",
-          "img": 'brugis.png',
-          "message": "Le dernier orthophotoplan 2019 est désormais accessible via BruGIS."
-      },
-      {
-          "datestart": "2019-09-01",
-          "dateend": "2020-10-01",
-          "defcon": "5",
-          "title": "Captage d'eau souterraine à Bruxelles",
-          "img": 'brugis.png',
-          "message": "Bruxelles Environnement possède une carte des captages d'eau souterraine. Vous retrouverez celle-ci sous Bruxelles Environnement - Données hydrographiques."
-      },
-      {
-          "datestart": "2019-09-01",
-          "dateend": "2020-10-01",
-          "defcon": "5",
-          "title": "Plan de qualité paysagère et urbanistique",
-          "img": 'brugis.png',
-          "message": "Perspective propose une nouvelle carte reprenant le plan de qualité paysagère et urbanistique. Vous le trouverez sous Perspective.brussels."
-      },
-      {
-          "datestart": "2019-05-01",
-          "dateend": "2020-01-01",
-          "defcon": "5",
-          "title": "BruGIS se dote d'une nouvelle page!",
-          "img": 'brugis.png',
-          "message": "BruGIS possède dorénavant une page d'information. Vous y trouverez les news de BruGIS, des renseignements techniques quant à nos webservices, ainsi qu'un panneau de contact. Revenez couramment consulter cette page afin d'être au courant des nouveautés, que ce soit à propos de nouvelles couches ajoutées à la plateforme qu'une modification d'un outil."
-      },
-      {
-        "datestart": "2019-05-01",
-        "dateend": "2019-09-01",
-        "defcon": "5",
-        "title": "Des isohypses dans BruGIS!",
-        "img": 'brugis.png',
-        "message": "La topographie bruxelloise peut être visualisée dès à présent à l'aide de courbes de niveau (ou isohypses). Vous trouverez cette nouvelle couche dans les fonds de plan."
-      },
-      {
-        "datestart": "2019-05-01",
-        "dateend": "2019-09-01",
-        "defcon": "5",
-        "title": "La topographie au format raster!",
-        "img": 'brugis.png',
-        "message": "En complément des courbes de niveaux, un modèle numérique de terrain est disponible. Vous trouverez cette nouvelle couche dans les fonds de plan."
-      },
-      {
-        "datestart": "2019-06-20",
-        "dateend": "2019-09-01",
-        "defcon": "5",
-        "title": "Interface graphique améliorée!",
-        "img": 'brugis.png',
-        "message": "Les outils d'urbanalyse, mes cartes, et d'autres ont été améliorés afin d'en faciliter l'usage. Les anciens textes sont dorénavant remplacés par des icônes plus faciles à utiliser et à comprendre. D'autres adaptations mineures ont été réalisées, entre autre dans le catalogue."
-      },
-      {
-        "datestart": "2019-06-25",
-        "dateend": "2019-09-01",
-        "defcon": "5",
-        "title": "L'outil Mes Cartes se libère!",
-        "message": "L'outil Mes Cartes se dote d'une fenêtre libre, ce qui vous permettra d'ajuster une dernière fois votre carte avant de la sauver."
+        "message": "Dans le courant des jeudi 11 juin et vendredi 12 juin, BruGIS migrera d'infrastructure. Cette mise à jour d'ampleur pourrait entraîner des difficultés d'accès à notre site et webservices durant ces journées."
       }
     ]
 };
