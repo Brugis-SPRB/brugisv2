b3fbeef5a0bc403df4be9351ad34418bdd4fceb4
Grégoire Vandenschrick <gregvds0@gmail.com>
2020-06-17 11:29:41 +0200

    new news

--- news/brugisnews_fr-FR.js
+++ news/brugisnews_fr-FR.js
@@ -9,6 +9,38 @@ const news = {
         "title": "Migration d'infrastructure BruGIS",
         "img": 'brugis.png',
         "message": "Dans le courant des jeudi 11 juin et vendredi 12 juin, BruGIS migrera d'infrastructure. Cette mise à jour d'ampleur pourrait entraîner des difficultés d'accès à notre site et webservices durant ces journées."
+      },
+      {
+        "datestart": "2020-06-15",
+        "dateend": "2020-09-01",
+        "defcon": "5",
+        "title": "Une nouvelle adresse pour BruGIS",
+        "img": 'brugis.png',
+        "message": "BruGIS a changé d'adresse. Vous retrouverez notre site et l'ensemble des webservices sous l'adresse https://gis.urban.brussels/."
+      },
+      {
+        "datestart": "2020-06-15",
+        "dateend": "2020-09-01",
+        "defcon": "5",
+        "title": "Une nouvelle carte des permis d'urbanisme",
+        "img": 'brugis.png',
+        "message": "Les permis d'urbanisme sont dorénavant visibles sous BruGIS. Vous trouverez la couche sous Bruxelles Urbanisme et Patrimoine/Urbanisme/Permis d'urbanisme."
+      },
+      {
+        "datestart": "2020-07-01",
+        "dateend": "2020-09-01",
+        "defcon": "5",
+        "title": "L'ensemble du Plan Régional de développement Durable visible sous BruGIS",
+        "img": 'brugis.png',
+        "message": "Toutes les cartes du PRDD sont dorénavant visibles sous BruGIS. Vous trouverez celles-ci sous perspective.brussels/PRDD/."
+      },
+      {
+        "datestart": "2020-06-15",
+        "dateend": "2020-07-01",
+        "defcon": "5",
+        "title": "Outil d'impression indisponible actuellement dans BruGIS",
+        "img": 'brugis.png',
+        "message": "L'outil d'impression des cartes est actuellement non fonctionnel sous BruGIS. Nous vous présentons nos excuses pour ce désagrément. Nous espérons rétablir la situation rapidement."
       }
     ]
 };
--- news/brugisnews_nl-NL.js
+++ news/brugisnews_nl-NL.js
@@ -8,6 +8,38 @@ const news = {
               "title": "Migratie van BruGIS-infrastructuur",
               "img": 'brugis.png',
               "message": "Op donderdag 11 en vrijdag 12 juni verandert BruGIS van infrastructuur. Deze grootschalige update kan tijdens deze dagen mogelijks leiden tot problemen bij het openen van de site en het gebruik van de webservices."
+      },
+      {
+        "datestart": "2020-06-15",
+        "dateend": "2020-09-01",
+        "defcon": "5",
+        "title": "Een nieuw adres voor BruGIS",
+        "img": 'brugis.png',
+        "message": "BruGIS is van adres veranderd. U kunt onze site en alle webservices vanaf nu terugvinden op het adres https://gis.urban.brussels/."
+      },
+      {
+        "datestart": "2020-06-15",
+        "dateend": "2020-09-01",
+        "defcon": "5",
+        "title": "Een nieuwe kaart met de stedenbouwkundige vergunningen",
+        "img": 'brugis.png',
+        "message": "De stedenbouwkundige vergunningen zijn vanaf nu zichtbaar op BruGIS. U vindt de laag terug onder Brussel Stedenbouw en Erfgoed / Stedenbouw / Stedenbouwkundige vergunning."
+      },
+      {
+        "datestart": "2020-07-01",
+        "dateend": "2020-09-01",
+        "defcon": "5",
+        "title": "Het volledige Gewestelijke Plan voor Duurzame Ontwikkeling op BruGIS",
+        "img": 'brugis.png',
+        "message": "Alle kaarten van het GDPO zijn vanaf nu zichtbaar op BruGIS. U vindt deze terug onder perspective.brussels/GPDO/."
+      },
+      {
+        "datestart": "2020-06-15",
+        "dateend": "2020-07-01",
+        "defcon": "5",
+        "title": "Afdruktool momenteel niet beschikbaar in BruGIS",
+        "img": 'brugis.png',
+        "message": "De kaartafdruktool is momenteel niet functioneel onder BruGIS. We verontschuldigen ons voor dit ongemak en hopen de situatie snel te herstellen."
       }
     ]
 };
