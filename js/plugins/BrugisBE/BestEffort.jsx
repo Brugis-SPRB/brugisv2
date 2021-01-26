bfa05ebf2c8ca07b273f2677f9479f2b251351b8
Grégoire Vandenschrick <gregvds0@gmail.com>
2021-01-26 15:45:51 +0100

    logo attempt 8

--- js/plugins/BrugisBE/BestEffort.jsx
+++ js/plugins/BrugisBE/BestEffort.jsx
@@ -24,7 +24,7 @@ class BestEffort extends React.Component {
         return (
             <div id={this.props.id} style={this.props.style}>
                 <label style={{ verticalAlign: 'middle'}}>
-                  <img style={{ height: '30px', padding: '3px', paddingLeft: '10px', paddingRight: '10px', verticalAlign: 'bottom', marginRight: '10px', backgroundColor: '#345d6f'}} src={uBrugisLogo} responsive/>
+                  <img style={{ height: '30px', padding: '3px', paddingLeft: '10px', paddingRight: '10px', marginRight: '10px', backgroundColor: '#345d6f'}} src={uBrugisLogo} responsive/>
                   <Message style={{ verticalAlign: 'super'}} msgId="best-effort" />
                   <a href="mailto:brugis@urban.brussels">
                     <Message style={{ verticalAlign: 'baseline'}} msgId="BruGISTeam" />
